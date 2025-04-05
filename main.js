c5324ef5a863c6146f15d461978d07cddd9f550e
NataliyaStavtseva <stav.tasha@gmail.com>
2025-04-05 17:50:29 +0300

    added page with chat

---
+++ main.js
@@ -0,0 +1,23 @@
+document.querySelector('.form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Предотвращаем перезагрузку страницы
+
+
+const nameInput = document.querySelector('.name-input');
+const messageInput = document.querySelector('.message-input');
+const username = nameInput.value.trim();
+const massage = messageInput.value.trim();
+
+
+if (username && massage) {
+const chatBox = document.querySelector('.message');
+const newMessage = document.createElement('div');
+newMessage.classList.add('chat-message');
+newMessage.innerHTML = `<span>${username}:</span> ${massage}`;
+
+chatBox.appendChild(newMessage);
+chatBox.scrollTop = chatBox.scrollHeight;
+
+username.value = '';
+message.value = '';
+}
+});
